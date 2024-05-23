084b1b9c30178ddbe15b89584a62cf5e17e0521f
Sonu Subedi <sonusubedi01@gmail.com>
2024-05-23 14:29:52 -0400

    pull blog posts from local storage and display

---
+++ assets/js/blog.js
@@ -0,0 +1,27 @@
+
+const blogPostsSectionEl = document.querySelector('#blog-posts');
+const storedBlogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+
+for (let blogPost of storedBlogPosts) {
+    const articleEl = document.createElement('article');
+    articleEl.className = 'box';
+
+    const titleEl = document.createElement('h2');
+    titleEl.textContent = blogPost.title;
+
+    const contentEl = document.createElement('p');
+    contentEl.textContent = blogPost.content;
+    contentEl.className = 'blog-content'
+
+    const authorEl = document.createElement('p');
+    authorEl.className = 'blog-author'
+    authorEl.textContent = `Posted by: ${blogPost.username}`;
+
+    articleEl.appendChild(titleEl);
+    articleEl.appendChild(contentEl);
+    articleEl.appendChild(authorEl);
+
+    blogPostsSectionEl.appendChild(articleEl);
+
+}
+
