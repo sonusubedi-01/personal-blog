449a9059256b4ec7fa28b0d0a4b4b290205c2cb4
Sonu Subedi <sonusubedi01@gmail.com>
2024-05-23 13:45:24 -0400

    add a light mode/dark mode toggle in landing page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,11 @@
-console.log("logic");
+const themeToggler = document.querySelector('.theme-toggler');
+
+themeToggler.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains("dark-theme")) {
+        themeToggler.textContent = '🌑';
+
+    } else {
+        themeToggler.textContent = '☀️';
+    }
+})
